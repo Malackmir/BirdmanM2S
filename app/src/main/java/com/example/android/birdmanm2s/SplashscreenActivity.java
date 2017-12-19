3fccaf14f2947df794b076a6465e7c58faf9504c
Kmir <kcamalack@gmail.com>
2017-12-19 23:45:41 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/birdmanm2s/SplashscreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.birdmanm2s;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
